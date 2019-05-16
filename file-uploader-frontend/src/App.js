26782b896c52c6a264d983ece58c90095826c956
BauerItSolutions <email.fabiopereira@gmail.com>
2019-05-16 22:29:22 +1000

    First Commit

---
+++ file-uploader-frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import GlobalStyle from './Styles/global'
+import { Container, Content } from './styles'
+import Upload from './components/Upload'
+
+
+class App extends Component {
+  render(){
+    return (  
+      <Container>
+        <Content><Upload /></Content>
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ file-uploader-frontend/src/components/Upload/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import Dropzone from 'react-dropzone';
+
+import { DropContainer, UploadMessage } from './styles'
+
+export default class Upload extends Component {
+  render() {
+    return (
+      <Dropzone accept="image/*" onDropAccepted={() => { }}>
+        {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
+          <DropContainer
+            {...getRootProps()}
+            isDragActive={isDragActive}
+            isDragReject={isDragReject}
+          >
+            <input {...getInputProps()} />     
+            Drop your files here 
+          </DropContainer>
+        )}
+      </Dropzone>
+    );
+  }
+}
